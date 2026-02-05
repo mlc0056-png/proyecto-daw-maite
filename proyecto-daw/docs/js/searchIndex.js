424a9741a321c9a87c40d3d1061bc769a88fe2f7
maite <mlc0056@alu.medac.es>
2026-02-05 22:25:23 +0100

    Generada documentación automática HTML con PHPDocumentor

---
+++ proyecto-daw/docs/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Conexion",
+            "name": "Conexion",
+            "summary": "",
+            "url": "classes/Conexion.html"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003Aconectar\u0028\u0029",
+            "name": "conectar",
+            "summary": "",
+            "url": "classes/Conexion.html#method_conectar"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024host",
+            "name": "host",
+            "summary": "",
+            "url": "classes/Conexion.html#property_host"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024usuario",
+            "name": "usuario",
+            "summary": "",
+            "url": "classes/Conexion.html#property_usuario"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024password",
+            "name": "password",
+            "summary": "",
+            "url": "classes/Conexion.html#property_password"
+        },                {
+            "fqsen": "\\Conexion\u003A\u003A\u0024db",
+            "name": "db",
+            "summary": "",
+            "url": "classes/Conexion.html#property_db"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
